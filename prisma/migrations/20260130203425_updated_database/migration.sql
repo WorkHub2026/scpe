317dc3f6d3f99438d8eb49aef49c2e0f7697c0ef
mohamein <ameinismail3144@gmail.com>
2026-01-30 12:39:58 -0800

    updated database

---
+++ prisma/migrations/20260130203425_updated_database/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "Ministry" ADD COLUMN     "facebook_url" TEXT,
+ADD COLUMN     "instagram_url" TEXT,
+ADD COLUMN     "linkedin_url" TEXT,
+ADD COLUMN     "twitter_url" TEXT,
+ADD COLUMN     "youtube_url" TEXT;
